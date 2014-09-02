1472d1d71fc05af89b2858ba0502bf890bfd3fe2
Cundong <cundong.liu@gmail.com>
2014-09-02 16:57:18 +0800

    V1.2
    将项目改造为开源Library，增加签名校验逻辑。

--- test/gen/com/example/test/R.java
+++ test/gen/com/example/test/R.java
@@ -17,15 +17,21 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
-        public static final int button1=0x7f080000;
+        public static final int action_settings=0x7f080007;
+        public static final int github_btn=0x7f080004;
+        public static final int progressView1=0x7f080005;
+        public static final int start_btn=0x7f080003;
+        public static final int textview1=0x7f080000;
+        public static final int textview2=0x7f080001;
+        public static final int textview3=0x7f080002;
+        public static final int textview4=0x7f080006;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -34,9 +40,14 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f040001;
+        public static final int app_name=0x7f040000;
+        public static final int demo_github=0x7f040007;
+        public static final int demo_info1=0x7f040003;
+        public static final int demo_info2=0x7f040004;
+        public static final int demo_info3=0x7f040005;
+        public static final int demo_start=0x7f040006;
+        public static final int hello_world=0x7f040002;
     }
     public static final class style {
         /** 
@@ -59,11 +70,33 @@ public final class R {
         res/values-v11/styles.xml on API 14+ devices.
     
  API 14 theme customizations can go here. 
+
+        Base application theme, dependent on API level. This theme is replaced
+        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+    
+
+            Theme customizations available in newer API levels can go in
+            res/values-vXX/styles.xml, while customizations related to
+            backward-compatibility can go here.
+        
+
+        Base application theme for API 11+. This theme completely replaces
+        AppBaseTheme from res/values/styles.xml on API 11+ devices.
+    
+ API 11 theme customizations can go here. 
+
+        Base application theme for API 14+. This theme completely replaces
+        AppBaseTheme from BOTH res/values/styles.xml and
+        res/values-v11/styles.xml on API 14+ devices.
+    
+ API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f050000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
+ Application theme. 
+ All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f050001;
     }
 }
--- test/src/com/example/test/MainActivity.java
+++ test/src/com/example/test/MainActivity.java
@@ -1,10 +1,6 @@
 package com.example.test;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -13,48 +9,91 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.cundong.apkpatch.utils.ApkUtils;
+import com.cundong.apkpatch.utils.SignUtils;
 import com.cundong.utils.PatchUtils;
 
-/**
- * Ò»¸öÀı×Ó£º ¼ÙÉèÊÖ»úÉÏÒÑ¾­°²×°ÁËWeiboV3£¬µã»÷¡°Éı¼¶¼ì²â¡±°´Å¥
- * 
- */
 public class MainActivity extends Activity {
 
-	private String PATH = Environment.getExternalStorageDirectory()
-			+ File.separator;
-	private String oldapk_filepath = "WeiboV3.apk";
+	private static final String TEST_PACKAGENAME = "com.sina.weibo";
+	private static final String PATH = Environment
+			.getExternalStorageDirectory() + File.separator;
 
-	private String newapk_savepath = "cundong.apk";
+	private String mOldApkName = "weiboV4.5.0.apk";
+	private String mNewApkName = "weiboOldtoNew.apk";
+	private String mPatchPath = "weiboPatch.apk";
+	
+	private ProgressBar mProgressBar;
+	private TextView mResultView;
+	private Button mStartButton, mGithubButton;
 
-	private String patchpath = "weibopatch.apk";
-
-	private Button mButton;
-
+	private long mBeginTime = 0;
+	private long mEndTime = 0;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		//
-		mButton = (Button) findViewById(R.id.button1);
-		mButton.setOnClickListener(new OnClickListener() {
+		mProgressBar = (ProgressBar) findViewById(R.id.progressView1);
+		mResultView = (TextView) findViewById(R.id.textview4);
+		mStartButton = (Button) findViewById(R.id.start_btn);
+		mGithubButton = (Button) findViewById(R.id.github_btn);
+		
+		mStartButton.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View arg0) {
-				Toast.makeText(MainActivity.this, "ĞÂapkºÏ³ÉÖĞ£¬ÇëµÈ´ı...",
-						Toast.LENGTH_LONG).show();
-				new Thread(new PatchThread()).start();
+
+				mStartButton.setClickable(false);
+				mResultView.setText("");
+				mProgressBar.setVisibility(View.VISIBLE);
+				
+				Toast.makeText(MainActivity.this, "æ–°apkåˆæˆä¸­ï¼Œè¯·ç­‰å¾…...", Toast.LENGTH_LONG).show();
+				
+				mBeginTime = System.currentTimeMillis();
+				
+				new PatchThread().start();
 			}
 		});
+		
+		mGithubButton.setOnClickListener(new OnClickListener() {
 
-		Log.i("@Cundong", "-->" + PATH);
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent("android.intent.action.VIEW", Uri.parse("https://github.com/cundong/SmartAppUpdates"));
+				startActivity(intent);
+			}
+		});
+	}
+
+	class PatchThread extends Thread {
+
+		@Override
+		public void run() {
+
+			boolean copyApkSuccess = ApkUtils.copySourceApk(TEST_PACKAGENAME, PATH + mOldApkName);
+
+			if (copyApkSuccess) {
+
+				int patchResult = PatchUtils.patch(PATH + mOldApkName, PATH + mNewApkName, PATH + mPatchPath);
+
+				if (patchResult == 0) {
+					mHandler.sendEmptyMessage(0);
+				} else {
+					mHandler.sendEmptyMessage(-1);
+				}
+			} else {
+				mHandler.sendEmptyMessage(-9999);
+			}
+		}
 	}
 
 	Handler mHandler = new Handler() {
@@ -65,134 +104,43 @@ public class MainActivity extends Activity {
 
 			int what = msg.what;
 
-			if (what == 0) {
+			mStartButton.setClickable(true);
+			mProgressBar.setVisibility(View.GONE);
+
+			if (what == -9999) {
+				
 				Toast.makeText(MainActivity.this,
-						"ĞÂapkÒÑºÏ³É³É¹¦£¬" + PATH + newapk_savepath,
+						"æ— æ³•è·å–packageNameä¸º" + TEST_PACKAGENAME,
 						Toast.LENGTH_LONG).show();
+				
+			} else if (what == 0) {
+
+				String s1 = SignUtils.getUnInstalledApkSignature(PATH
+						+ mNewApkName);
+				
+				String s2 = SignUtils.InstalledApkSignature(MainActivity.this,
+						TEST_PACKAGENAME);
+
+				if (s1 != null && s1.equals(s2)) {
+					Toast.makeText(MainActivity.this,
+							"æ–°apkå·²åˆæˆæˆåŠŸï¼š" + PATH + mNewApkName, Toast.LENGTH_LONG)
+							.show();
+					
+					mEndTime = System.currentTimeMillis();
+					mResultView.setText("è€—æ—¶: " + (mEndTime-mBeginTime) + "ms");
+					ApkUtils.installApk(MainActivity.this, PATH + mNewApkName);
+				} else {
+					Toast.makeText(MainActivity.this, "æ–°apkå·²åˆæˆå¤±è´¥ï¼Œç­¾åä¸ä¸€è‡´",
+							Toast.LENGTH_LONG).show();
+				}
 			} else {
-				Toast.makeText(MainActivity.this, "ĞÂapkÒÑºÏ³ÉÊ§°Ü",
+				Toast.makeText(MainActivity.this, "æ–°apkå·²åˆæˆå¤±è´¥",
 						Toast.LENGTH_LONG).show();
 			}
-
-			installAPK(PATH + "WeiboV3.apk");
 		}
 	};
 
-	class PatchThread implements Runnable {
-
-		@Override
-		public void run() {
-
-			backupApplication("com.sina.weibo", PATH + "WeiboV3.apk");
-
-			int ret = PatchUtils.patch(PATH + oldapk_filepath, PATH
-					+ newapk_savepath, PATH + patchpath);
-
-			mHandler.sendEmptyMessage(ret);
-		}
-	}
-
 	static {
-		System.loadLibrary("apkpatch");
-	}
-
-	/**
-	 * °²×°APK
-	 * 
-	 * @param apkUrl
-	 */
-	private void installAPK(String apkUrl) {
-		Intent intent = new Intent(Intent.ACTION_VIEW);
-		intent.setDataAndType(Uri.parse("file://" + apkUrl),
-				"application/vnd.android.package-archive");
-		startActivity(intent);// °²×°
-	}
-
-	/**
-	 * 
-	 * @Description ½«appÓÉdata/appÄ¿Â¼¿½±´µ½sd¿¨ÏÂµÄÖ¸¶¨Ä¿Â¼ÖĞ
-	 * 
-	 * @param appId
-	 *            Ó¦ÓÃ³ÌĞòµÄIDºÅ£¬Èçcom.wondertek.jttxl
-	 * 
-	 * @param dest
-	 *            ĞèÒª½«Ó¦ÓÃ³ÌĞò¿½±´µÄÄ¿±êÎ»ÖÃ
-	 * 
-	 * @date 2013-7-24 ÏÂÎç3:32:12
-	 */
-	private String backupApplication(String packageName, String dest) {
-
-		if (packageName == null || packageName.length() == 0
-
-		|| dest == null || dest.length() == 0) {
-			return "illegal parameters";
-		}
-
-		// check file /data/app/appId-1.apk exists
-
-		String apkPath = "/data/app/" + packageName + "-1.apk";
-
-		File apkFile = new File(apkPath);
-
-		if (apkFile.exists() == false) {
-			return apkPath + " doesn't exist!";
-		}
-
-		FileInputStream in = null;
-
-		try {
-			in = new FileInputStream(apkFile);
-
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-			return e.getMessage();
-		}
-
-		// create dest folder if necessary
-
-		int i = dest.lastIndexOf('/');
-
-		if (i != -1) {
-			File dirs = new File(dest.substring(0, i));
-			dirs.mkdirs();
-			dirs = null;
-		}
-
-		// do file copy operation
-
-		byte[] c = new byte[1024];
-
-		int slen;
-
-		FileOutputStream out = null;
-
-		try {
-			out = new FileOutputStream(dest);
-
-			while ((slen = in.read(c, 0, c.length)) != -1)
-				out.write(c, 0, slen);
-		} catch (IOException e) {
-			e.printStackTrace();
-			return e.getMessage();
-		} finally {
-			if (out != null)
-				try {
-					out.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-					return e.getMessage();
-				}
-			if (in != null) {
-				try {
-					in.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-					return e.getMessage();
-				}
-			}
-		}
-
-		return "success";
-
+		System.loadLibrary("ApkPatchLibrary");
 	}
 }

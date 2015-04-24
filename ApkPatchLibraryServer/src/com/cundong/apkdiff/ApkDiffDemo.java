21643c0375b14d7ccea2580c49eb44e5ae838a9b
cundong <cundong.liu@gmail.com>
2015-04-24 09:50:40 +0800

    code refactoring

--- ApkPatchLibraryServer/src/com/cundong/apkdiff/ApkDiffDemo.java
+++ ApkPatchLibraryServer/src/com/cundong/apkdiff/ApkDiffDemo.java
@@ -4,7 +4,7 @@ import com.cundong.common.Constants;
 import com.cundong.utils.DiffUtils;
 
 /**
- * 类说明：  apk包，查分实例
+ * 类说明：  apk包，差分实例
  * 
  * @author  Cundong
  * @date    2014-9-6
@@ -16,14 +16,14 @@ public class ApkDiffDemo {
 		
 		long start = System.currentTimeMillis();
 		
-		System.out.println("开始生成差异包，请等待...");
+		System.out.println("开始生成差分包，请等待...");
 
 		int genDiff = DiffUtils.genDiff(Constants.OLD_APK, Constants.NEW_APK,
 				Constants.PATCH_FILE);
 
 		long end = System.currentTimeMillis();
 
-		System.out.println("生成差异包成功：" + Constants.PATCH_FILE + "，耗时："
+		System.out.println("生成差分包成功：" + Constants.PATCH_FILE + "，耗时："
 				+ (end - start) / 1000 + "秒, result=" + genDiff);
 	}
 
